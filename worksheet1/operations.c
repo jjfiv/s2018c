c5ab0eaba69224f890fabe662b5bde39e1b3f5fc
John Foley <jfoley@cs.umass.edu>
2018-01-30 14:29:41 +0000

    Add worksheet1

---
+++ worksheet1/operations.c
@@ -0,0 +1,23 @@
+#include "operations.h"
+
+// This is a function named "addition" that takes two arguments, "op1" and
+// "op2" and returns their sum.
+int addition(int op1, int op2) {
+  return op1 + op2;
+}
+
+// Create a function for subtraction that is named "subtraction", takes two
+// ints, and returns the difference. (You should subtract the SECOND number
+// from the FIRST)
+
+// Create a function for multiplication that is named "multiplication", takes
+// two ints, and returns the product.
+
+// Create a function for division that is named "division", takes two ints, and
+// returns the quotient. (You should divide the first number by the second
+// number. Keep in mind we are using integer division, so all answers will be
+// automatically rounded down.)
+
+//NOTE: you cannot test your code until you have created the subtraction,
+//multiplication, and division functions (though they do not need to work).
+
---
+++ worksheet1/operations.h
@@ -0,0 +1,9 @@
+#ifndef _OPERATIONS_H
+#define _OPERATIONS_H
+
+int addition(int, int);
+int subtraction(int, int);
+int multiplication(int, int);
+int division(int, int);
+
+#endif
